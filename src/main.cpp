cc8fe7175a6d452ece54705c7e6513b2b50c0034
Washington Kigani Kamadi <washingtronics@gmail.com>
2024-11-02 09:02:18 +0300

    add standby pin
    Signed-off-by: Washington Kigani Kamadi <washingtronics@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ MPU6050 mpu6050(Wire);
 
 // Motor Control
 #define enA 9
-#define in1 4
+#define in1 6
 #define in2 7
 
 #define enB 10
@@ -16,10 +16,11 @@ MPU6050 mpu6050(Wire);
 #define standbypin 8
 
 // For PID Controller 
-float Kp = 1;             // (P)roportional Tuning Parameter
-float Ki = 2;             // (I)ntegral Tuning Parameter        
-float Kd = 3;             // (D)erivative Tuning Parameter
-float targetAngle = 0;  // Can be adjusted according to centre of gravity 
+float Kp = 24;             // (P)roportional Tuning Parameter
+float Ki = 3;             // (I)ntegral Tuning Parameter        
+float Kd = 15;             // (D)erivative Tuning Parameter
+float targetAngle = -1.5;  // Can be adjusted according to centre of gravity 
+float time, currentTime, previousTime;
 
 float lastpitch = 0;          // Keeps track of error over time
 float iTerm;              // Used to accumulate error (integral)
@@ -73,24 +74,27 @@ void MotorDriver(int PIDValue){
 }
 
 int PID(){
+  currentTime = millis();
   // Calculate pitch
   float pitch = mpu6050.getAngleX();
   
   // Calculate Error
   float error = targetAngle - pitch;
+  if (abs(error) <= 1.5) error = 0;
 
   // Calculate PID terms
   float pTerm = Kp * error;
-  iTerm += Ki * error * 10;
-  float dTerm = Kd * (pitch - lastpitch) / 10;
+  time = currentTime - previousTime;
+  iTerm += Ki * error * time;
+  float dTerm = Kd * (pitch - lastpitch) / time;
   lastpitch = pitch;
 
   // Obtain PID output value
-  float PIDValue = pTerm + iTerm - dTerm;
+  float PIDValue = pTerm + iTerm + dTerm;
 
   // Cap values so be able to send the correct PWM signal to the motors
   if (PIDValue > 255) PIDValue = 255;
   else if (PIDValue < -255) PIDValue = -255;
-  
+  previousTime = millis();
   return int(PIDValue);
 }
